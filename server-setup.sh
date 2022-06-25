7576d634ee3bf5cadb72d9eede4b80d102041472
Franek <fr.strzezek@gmail.com>
2022-06-25 18:02:55 +0200

    `

--- server-setup.sh
+++ server-setup.sh
@@ -16,7 +16,7 @@ else
 fi
 
 # install stuff that I want
-$PM -y install git zsh docker docker-compose wget
+sudo $PM -y install git zsh docker docker-compose wget
 # get newest relase of btop from https://github.com/aristocratos/btop/releases
 regex='^href.*\/.*\/(v[0-9]\.[0-9]\.[0-9])'
 for v in $(curl https://github.com/aristocratos/btop/releases | grep releases/download); do
@@ -41,23 +41,24 @@ done
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # install zsh-autosuggestions and zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-
-# copy my zshrc to home
-cp .zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.zsh/zsh-syntax-highlighting
 
 # copy contents of cpu-scripts to $HOME/.local/bin
 cp -r cpu-scripts /usr/bin  
 
 # clone update-golang and insall go
-git clone https://github.com/udhos/update-golang ~/.update-golang
-cd ~/.update-golang
+git clone https://github.com/udhos/update-golang $HOME/.update-golang
+cd $HOME/.update-golang
 sudo ./update-golang.sh
 cd $dir
 
 # install CompileDeamon from https://github.com/fr-str/CompileDaemon
-git clone https://github.com/fr-str/CompileDaemon ~/.CompileDaemon
-cd ~/.CompileDaemon
+git clone https://github.com/fr-str/CompileDaemon $HOME/.CompileDaemon
+cd $HOME/.CompileDaemon
 go build
-cp CompileDaemon ~/go/bin/CompileDaemon
+cp CompileDaemon $HOME/go/bin/CompileDaemon
+
+# copy my zshrc to home
+cp .zshrc $HOME/.zshrc
+source $HOME/.zshrc
