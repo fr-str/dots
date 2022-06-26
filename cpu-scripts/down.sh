3b901cd3cd5c942225f465fba4f0ffb424af344c
Franek <fr.strzezek@gmail.com>
2022-06-26 12:46:10 +0200

    `

--- cpu-scripts/down.sh
+++ cpu-scripts/down.sh
@@ -1,5 +1,9 @@
-cpufreq-set -g ondemand
-cpufreq-set -d 1400000
-cpufreq-set -f 1400000
+#!/bin/bash
+for i in {0..5};do
+    cpufreq-set -c $i -g ondemand
+    cpufreq-set -c $i -u 1400000
+    cpufreq-set -c $i -d 1400000
+    cpufreq-set -c $i -f 1400000
+done
 sleep 0.1
 cat /proc/cpuinfo | grep 'cpu MHz'
