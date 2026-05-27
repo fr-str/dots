0f6f22b65d3fa117e7b95cb39e8cd4862634fb0c
Franciszek <fr.strzezek@gmail.com>
2026-05-28 00:10:36 +0200

    oil and timux-navigator

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+vim.pack.add({ "https://github.com/stevearc/oil.nvim" })
+require("oil").setup({
+	columns = { "icon" },
+	keymaps = {
+		["<C-h>"] = false,
+		["<M-h>"] = "actions.select_split",
+	},
+	view_options = {
+		show_hidden = true,
+	},
+})
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
---
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,5 @@
+vim.pack.add({ "https://github.com/christoomey/vim-tmux-navigator" })
+vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
